36b148eabfc8162cb5cbd1d0773b1b539be3c004
DNogueiraIPCA <dnogueira@ipca.pt>
2023-03-23 15:49:23 +0000

    aula 06

--- Sokoban_Projeto_01/Sokoban_Projeto_01/Game1.cs
+++ Sokoban_Projeto_01/Sokoban_Projeto_01/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Numerics;
 using System.Reflection.Emit;
@@ -9,18 +10,39 @@ using Vector2 = Microsoft.Xna.Framework.Vector2;
 
 namespace Sokoban_Projeto_01
 {
+    /*
+    * - WinScreen
+    * - MultipleLevels
+    * - Timer
+    * - max refresh count
+    */
+    //public enum Direction
+    //{
+    //    Up, Down, Left, Right // 0, 1, 2, 3
+    //}
+
+
     public class Game1 : Game
     {
+        //C:\Daniel\ipca\licenciatura\Tecnicas de Desenvolvimento de Videojogos\git\sokoban_Aula_03\Sokoban_Projeto_01\Sokoban_Projeto_01
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private int nrLinhas = 0;
         private int nrColunas = 0;
-        private SpriteFont font;
-        private char[,] level;
-        private Texture2D player, dot, box, wall; //Load images Texture 
-        int tileSize = 64; //potencias de 2 (operações binárias)
+        private SpriteFont font, arial12;
+        private Texture2D dot, box, wall; //Load images Texture 
+        //private Texture2D[] player;
         private Player sokoban;
+        //private char[,] level;
+        
+        public const int tileSize = 64; //potencias de 2 (operações binárias)
 
+        //public Direction direction = Direction.Down;
+        public char[,] level;
+        public List<Point> boxes;
+
+     
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -32,11 +54,14 @@ namespace Sokoban_Projeto_01
         {
             // TODO: Add your initialization logic here
 
-            base.Initialize();
             LoadLevel("level1.txt");
-            _graphics.PreferredBackBufferHeight = tileSize * level.GetLength(1); //definição da altura
+            _graphics.PreferredBackBufferHeight = tileSize * (1 + level.GetLength(1)); //definição da altura
             _graphics.PreferredBackBufferWidth = tileSize * level.GetLength(0); //definição da largura
             _graphics.ApplyChanges(); //aplica a atualização da janela
+
+            sokoban.LoadContents();
+
+            base.Initialize();
 
         }
 
@@ -45,10 +70,17 @@ namespace Sokoban_Projeto_01
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             // Use the name of your sprite font file here instead of 'File'.
             font = Content.Load<SpriteFont>("File");
-            player = Content.Load<Texture2D>("Character4");
+            arial12 = Content.Load<SpriteFont>("arial12");
+            //player = Content.Load<Texture2D>("Character4");
             dot = Content.Load<Texture2D>("EndPoint_Blue");
             box = Content.Load<Texture2D>("Crate_Brown");
             wall = Content.Load<Texture2D>("Wall_Brown");
+            
+            //player = new Texture2D[4];
+            //player[(int) Direction.Down] = Content.Load<Texture2D>("Character4");
+            //player[(int)Direction.Up] = Content.Load<Texture2D>("Character7");
+            //player[(int)Direction.Left] = Content.Load<Texture2D>("Character1");
+            //player[(int)Direction.Right] = Content.Load<Texture2D>("Character2");
 
 
             // TODO: use this.Content to load your game content here
@@ -60,6 +92,12 @@ namespace Sokoban_Projeto_01
                 Exit();
 
             // TODO: Add your update logic here
+
+            if (Keyboard.GetState().IsKeyDown(Keys.R)) Initialize(); // Game restart
+
+            if (Victory()) Exit(); // FIXME: Change current level
+
+            sokoban.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -73,6 +111,17 @@ namespace Sokoban_Projeto_01
             base.Draw(gameTime);
 
             _spriteBatch.Begin();
+            // Draw UI
+            _spriteBatch.DrawString(arial12, // Tipo de letra
+                                    "Tempo Decorrido = ",  // Texto
+                                    new Vector2(5, level.GetLength(1) * tileSize + 5), // Posição do texto
+                                    Color.White, // Cor da letra
+                                    0f, //Rotação
+                                    Vector2.Zero, // Origem
+                                    2f, // Escala
+                                    SpriteEffects.None, //FlipHorizontally, //Sprite effect
+                                    0); // Ordenar sprites
+
             _spriteBatch.DrawString(font, "O texto que quiser", new Vector2(0, 40), Color.Black);
             _spriteBatch.DrawString(font, $"Numero de Linhas = {nrLinhas}", new Vector2(0, 0), Color.Black);
             _spriteBatch.DrawString(font, $"Numero de Colunas = {nrColunas}", new Vector2(0, 20), Color.Black);
@@ -89,9 +138,9 @@ namespace Sokoban_Projeto_01
                         //case 'Y':
                         //    _spriteBatch.Draw(player, position, Color.White);
                         //    break;
-                        case '#':
-                            _spriteBatch.Draw(box, position, Color.White);
-                            break;
+                        //case '#':
+                        //    _spriteBatch.Draw(box, position, Color.White);
+                        //    break;
                         case '.':
                             _spriteBatch.Draw(dot, position, Color.White);
                             break;
@@ -101,28 +150,70 @@ namespace Sokoban_Projeto_01
                     }
                 }
             }
-            position.X = sokoban.Position.X * tileSize; //posição do Player
-            position.Y = sokoban.Position.Y * tileSize; //posição do Player
-            _spriteBatch.Draw(player, position, Color.White); //desenha o Player
-            _spriteBatch.End();
 
+            sokoban.Draw(_spriteBatch);
+
+            //position.X = sokoban.Position.X * tileSize; //posição do Player
+            //position.Y = sokoban.Position.Y * tileSize; //posição do Player
+            //_spriteBatch.Draw(player[(int) direction], position, Color.White); //desenha o Player
+
+            foreach (Point b in boxes)
+            {
+                position.X = b.X * tileSize;
+                position.Y = b.Y * tileSize;
+                _spriteBatch.Draw(box, position, Color.White);
+            }
+
+            _spriteBatch.End();
 
          }
 
+        public bool HasBox(int x, int y)
+        {
+            foreach (Point b in boxes)
+            {
+                if (b.X == x && b.Y == y) return true; // se a caixa tiver a mesma posição do Player
+            }
+            return false;
+        }
+        public bool FreeTile(int x, int y)
+        {
+            if (level[x, y] == 'X') return false;  // se for uma parede está ocupada
+            if (HasBox(x, y)) return false; // verifica se é uma caixa
+            return true;
+
+            /* The same as:    return level[x,y] != 'X' && !HasBox(x,y);   */
+        }
+
+        public bool Victory()
+        {
+            foreach (Point b in boxes) // pecorrer a lista das caixas
+            {
+                if (level[b.X, b.Y] != '.') return false; // verifica se há caixas sem pontos
+            }
+            return true;
+        }
         void LoadLevel(string levelFile)
         {
             string[] linhas = File.ReadAllLines($"Content/{levelFile}");  // "Content/" + level
             nrLinhas = linhas.Length;
             nrColunas = linhas[0].Length;
             level = new char[nrColunas, nrLinhas];
+            boxes = new List<Point>();
 
             for (int x = 0; x < nrColunas; x++)
             {
                 for (int y = 0; y < nrLinhas; y++)
                 {
-                    if (linhas[y][x] == 'Y')
+                    if (linhas[y][x] == '#')
                     {
-                        sokoban = new Player(x, y);
+                        boxes.Add(new Point(x, y));
+                        level[x, y] = ' '; // put a blank instead of the box '#'
+                    }
+
+                    else if (linhas[y][x] == 'Y')
+                    {
+                        sokoban = new Player(this, x, y);
                         level[x, y] = ' '; // put a blank instead of the sokoban 'Y'
                     }
                     else
--- Sokoban_Projeto_01/Sokoban_Projeto_01/Player.cs
+++ Sokoban_Projeto_01/Sokoban_Projeto_01/Player.cs
@@ -4,19 +4,166 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+//using System.Numerics;
+using System.Reflection.Metadata;
 
 namespace Sokoban_Projeto_01
 {
+    enum Direction
+    {
+        Up, Down, Left, Right // 0, 1, 2, 3
+    }
+
+
     public class Player
     {
         // Current player position in the matrix (multiply by tileSize prior to drawing)
         private Point position; //Point = Vector2, mas são inteiros
-        public Point Position => position; //auto função (equivalente a ter só get sem put)
+        private Game1 game; //reference from Game1 to Player
+        //private bool keysReleased = true;
+        private Direction direction = Direction.Down;
+        private int delta = 0;
+        private int speed = 1; // NOTA: tem de ser divisor de tileSize
+        private Vector2 directionVector;
 
-        public Player(int x, int y) //constructor que dada a as +osições guarda a sua posição
+        public Point Position => position; //auto função (equivalente a ter só get sem put)
+        
+        // Direction 
+        private Texture2D[][] sprites;
+
+        // Funções:
+        // - Draw(player)
+        // - LoadContents(player)
+
+        public Player(Game1 game1, int x, int y) //constructor que dada a as +osições guarda a sua posição
         {
             position = new Point(x, y);
+            game = game1;
+
         }
+
+        public void LoadContents()
+        {
+            sprites = new Texture2D[4][];
+            sprites[(int)Direction.Up] = new[] {
+                game.Content.Load<Texture2D>("Character7"),
+                game.Content.Load<Texture2D>("Character8"),
+                game.Content.Load<Texture2D>("Character9")  };
+            sprites[(int)Direction.Down] = new[] {
+                game.Content.Load<Texture2D>("Character4"),
+                game.Content.Load<Texture2D>("Character5"),
+                game.Content.Load<Texture2D>("Character6") };
+            sprites[(int)Direction.Left] = new[] {
+                game.Content.Load<Texture2D>("Character1"),
+                game.Content.Load<Texture2D>("Character10") };
+            sprites[(int)Direction.Right] = new[] {
+                game.Content.Load<Texture2D>("Character2"),
+                game.Content.Load<Texture2D>("Character3") };
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (delta > 0)
+            {
+                delta = (delta + speed) % Game1.tileSize;
+            }
+            else
+            {
+                Point lastPosition = position;
+                KeyboardState kState = Keyboard.GetState();
+                if ((kState.IsKeyDown(Keys.A)) || (kState.IsKeyDown(Keys.Left)))
+                {
+                    position.X--;
+                    direction = Direction.Left;
+                    delta = speed;
+                    // directionVector = new Vector2(-1, 0);
+                    directionVector = -Vector2.UnitX;
+                }
+                else if ((kState.IsKeyDown(Keys.W)) || (kState.IsKeyDown(Keys.Up)))
+                {
+                    position.Y--;
+                    direction = Direction.Up;
+                    delta = speed;
+                    directionVector = -Vector2.UnitY;
+                }
+                else if ((kState.IsKeyDown(Keys.S)) || (kState.IsKeyDown(Keys.Down)))
+                {
+                    position.Y++;
+                    direction = Direction.Down;
+                    delta = speed;
+                    directionVector = Vector2.UnitY;
+                }
+                else if ((kState.IsKeyDown(Keys.D)) || (kState.IsKeyDown(Keys.Right)))
+                {
+                        position.X++;
+                        direction = Direction.Right;
+                        delta = speed;
+                        directionVector = Vector2.UnitX;
+                }
+
+                // destino é caixa?
+                if (game.HasBox(position.X, position.Y))
+                {
+                        int deltaX = position.X - lastPosition.X;
+                        int deltaY = position.Y - lastPosition.Y;
+                        Point boxTarget = new Point(deltaX + position.X, deltaY + position.Y);
+                        //  se sim, caixa pode mover-se?
+                        if (game.FreeTile(boxTarget.X, boxTarget.Y))
+                        {
+                            for (int i = 0; i < game.boxes.Count; i++)
+                            {
+                                if (game.boxes[i].X == position.X && game.boxes[i].Y == position.Y)
+                                {
+                                    game.boxes[i] = boxTarget;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            position = lastPosition;
+                            delta = 0;
+                        }
+                }
+                else
+                {
+                    //  se não é caixa, se não está livre, parado!
+                    if (!game.FreeTile(position.X, position.Y))
+                    {
+                        position = lastPosition;
+                        delta = 0;
+                    }
+                }
+
+            }
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            // Point(1,1) => Vector(1,1) => Vector(64,64) => Vector(64,64) + delta * Vector(1,0)
+            // Vector(64 + delta, 64)
+            // 0,0 => 1,0
+            // 64,0
+            // 64 - (64 - 1) ==> 64 - 63 ==> 1
+            // 64 - (64 - 2) ==> 64 - 62 ==> 2
+            Vector2 pos = position.ToVector2() * Game1.tileSize;
+            int frame = 0;
+            if (delta > 0)
+            {
+                pos -= (Game1.tileSize - delta) * directionVector;
+                float animSpeed = 8f;
+                frame = (int)((delta / speed) % ((int)animSpeed * sprites[(int)direction].Length) / animSpeed);
+            }
+
+            //Rectangle rect = new Rectangle(Game1.tileSize * position.X, 
+            //                               Game1.tileSize * position.Y,
+            //                               Game1.tileSize, Game1.tileSize);
+            Rectangle rect = new Rectangle(pos.ToPoint(), new Point(Game1.tileSize));
+            sb.Draw(sprites[(int)direction][frame], rect, Color.White); //desenha o Player
+        }
+
     }
+
 }
 
